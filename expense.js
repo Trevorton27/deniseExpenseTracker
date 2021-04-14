80cef70b4d90824de445e6a27e195f76d1a22d8b
Trevor Mearns <spiral272@gmail.com>
2021-04-14 11:28:46 +0900

    code review finished

--- expense.js
+++ expense.js
@@ -1,93 +1,98 @@
 const expenseArray = JSON.parse(localStorage.getItem('expenseArray')) || [];
-const tableBody = document.getElementById('tblBody');
+const tableBody = document.getElementById('expenseTbl');
 
 const expenseDate = document.getElementById('dateInput');
 const expenseDescription = document.getElementById('descrInput');
 const expenseAmount = document.getElementById('amountInput');
 const expenseVendor = document.getElementById('vendorInput');
+
 const addBtn = document.getElementById('addBtn');
 
 addBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
+  if (isValidateForm()) {
+    alert('Please fill out all fields before submitting.');
+    return;
+  }
+
   expenseItem = {
-    id: Math.random(),
+    id: Date.now(),
     date: expenseDate.value,
     description: expenseDescription.value,
     amount: expenseAmount.value,
     vendor: expenseVendor.value
   };
 
-  validateForm()
-    ? alert('Please fill out all fields before submitting.')
-    : addExpense(expenseItem);
+  addExpense(expenseItem);
 
-  console.log(validateForm());
+  document.getElementById('form').reset();
 });
-
-console.log(validateForm());
 
 function addExpense(expense) {
   renderRow(expense);
   expenseArray.push(expense);
   pushToLocalStorage(expense);
 }
-function renderRow(expense) {
-  const tableRow = document.createElement('tr');
-  tableBody.appendChild(tableRow);
-
-  const dateCell = document.createElement('td');
-  dateCell.textContent = expense.date;
-  tableRow.appendChild(dateCell);
-
-  const descriptionCell = document.createElement('td');
-  descriptionCell.textContent = expense.description;
-  tableRow.appendChild(descriptionCell);
-
-  const amountCell = document.createElement('td');
-  amountCell.textContent = expense.amount;
-  tableRow.appendChild(amountCell);
-
-  const vendorCell = document.createElement('td');
-  vendorCell.textContent = expense.vendor;
-  tableRow.appendChild(vendorCell);
-
-  const deleteButton = document.createElement('button');
-  deleteButton.textContent = 'X';
-  deleteButton.setAttribute('id', 'deleteButton');
-  tableRow.appendChild(deleteButton);
-
-  deleteButton.addEventListener('click', (e) => {
-    e.preventDefault();
-    deleteExpenseRow(deleteButton, expense.id);
-  });
-
-  document.getElementById('form').reset();
-}
 
 function pushToLocalStorage(expense) {
   localStorage.setItem('expenseArray', JSON.stringify(expenseArray));
 }
 
-function validateForm() {
-  const isInputEmpty =
-    !expenseDate.value ||
-    !expenseDescription.value ||
-    !expenseAmount.value ||
-    !expenseDescription.value;
+function renderRow(expense) {
+  const tableRow = document.createElement('tr');
+  tableBody.appendChild(tableRow);
 
-  return isInputEmpty ? true : false;
+  const dateCell = createDateCell(expense);
+  tableRow.appendChild(dateCell);
 
-  //   if (
-  //     !expenseDate.value ||
-  //     !expenseDescription.value ||
-  //     !expenseAmount.value ||
-  //     !expenseVendor.value
-  //   ) {
-  //     return true;
-  //   } else {
-  //     return false;
-  //   }
+  const descriptionCell = createDescriptionCell(expense);
+  tableRow.appendChild(descriptionCell);
+
+  const amountCell = createAmountCell(expense);
+  tableRow.appendChild(amountCell);
+
+  const vendorCell = createVendorCell(expense);
+  tableRow.appendChild(vendorCell);
+
+  const deleteButton = createDeleteButton(expense);
+  tableRow.appendChild(deleteButton);
+}
+
+function createDateCell(expense) {
+  const dateCell = document.createElement('td');
+  dateCell.textContent = expense.date;
+  return dateCell;
+}
+
+function createDescriptionCell(expense) {
+  const descriptionCell = document.createElement('td');
+  descriptionCell.textContent = expense.description;
+  return descriptionCell;
+}
+
+function createAmountCell(expense) {
+  const amountCell = document.createElement('td');
+  amountCell.textContent = expense.amount;
+  return amountCell;
+}
+
+function createVendorCell(expense) {
+  const vendorCell = document.createElement('td');
+  vendorCell.textContent = expense.vendor;
+  return vendorCell;
+}
+
+function createDeleteButton(expense) {
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'X';
+  deleteButton.setAttribute('id', 'deleteButton');
+
+  deleteButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    deleteExpenseRow(deleteButton, expense.id);
+  });
+  return deleteButton;
 }
 
 function deleteExpenseRow(deleteButton, id) {
@@ -95,9 +100,19 @@ function deleteExpenseRow(deleteButton, id) {
   for (let i = 0; i < expenseArray.length; i++) {
     if (expenseArray[i].id === id) {
       expenseArray.splice(i, 1);
-      localStorage.setItem('expenseArray', JSON.stringify(expenseArray));
+      pushToLocalStorage(expenseArray);
     }
   }
+}
+
+function isValidateForm() {
+  const isInputEmpty =
+    !expenseDate.value ||
+    !expenseDescription.value ||
+    !expenseAmount.value ||
+    !expenseVendor.value;
+
+  return isInputEmpty ? true : false;
 }
 
 window.addEventListener('load', (e) => {
